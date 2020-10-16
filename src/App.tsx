bba1b1ebeb92e94752e853fdc4776acf977735ae
Jan Andersson <jan.andersson@gmail.com>
2020-10-16 21:33:29 +0200

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [filename, setFilename] = useState("https://johnearnest.github.io/chip8Archive/roms/danm8ku.ch8");
+  const [filename, setFilename] = useState(
+    'https://johnearnest.github.io/chip8Archive/roms/danm8ku.ch8'
+  )
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setFilename(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setFilename(event.target.value)
 
   const loadFile = () => {
     fetch(filename)
       .then((resp) => resp.arrayBuffer())
       .then((buffer) => new Uint8Array(buffer))
-      .then((arr) => alert(arr.byteLength));
-  };
+      .then((arr) => alert(arr.byteLength))
+  }
 
   return (
     <div className="App">
@@ -23,12 +26,17 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
