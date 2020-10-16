e4f5a5fb4ba765f222348ef0b818cd954d19bf08
Jan Andersson <jan.andersson@gmail.com>
2020-10-16 22:42:59 +0200

    Refactor view, autoload file

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import logo from './logo.svg'
 import { load } from './net'
-import { init, initWithData } from './ram'
+import { create, createWithData } from './memory'
 import './App.css'
 
 function App() {
-  let ram = init()
+  let mem = create()
 
-  const [filename, setFilename] = useState('')
+  const [filename, setFilename] = useState(
+    'https://johnearnest.github.io/chip8Archive/roms/octojam1title.ch8'
+  )
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilename(event.target.value)
   }
 
-  const loadFile = () => {
+  const loadFile = useCallback(() => {
     load(filename).then((data: Uint8Array) => {
-      ram = initWithData(data)
+      mem = createWithData(data)
     })
-  }
+  }, [])
+
+  useEffect(() => {
+    loadFile()
+  }, [loadFile])
 
   return (
     <div className="App">
       <header className="App-header">
-        <input value={filename} onChange={handleChange} />
-        <button onClick={loadFile}>Load</button>
+        <div className="file">
+          <input value={filename} onChange={handleChange} />
+          <button onClick={loadFile}>Load</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>RAM: {ram.length} bytes</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
